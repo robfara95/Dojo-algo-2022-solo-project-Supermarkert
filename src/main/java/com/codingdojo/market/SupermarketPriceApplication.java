1f5f4c4ff4d27d953bc298e3d4e219e44b5ef5bb
Robert Simon <robfara@gmail.com>
2022-03-10 13:24:22 -0500

    Version 0.0 - initial .gitignore and ide generated code

---
+++ src/main/java/com/codingdojo/market/SupermarketPriceApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketPriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingdojo/market/SupermarketPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketPriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
